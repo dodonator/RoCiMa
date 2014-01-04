b16f7331fcfdcbcaff4ddac32ffb33227de35a31
dodonator <dodo@dodo@chaosdorf.de>
2014-01-04 12:30:44 +0100

    test

--- ENI.W3.oS.oR.py
+++ ENI.W3.oS.oR.py
@@ -5,21 +5,40 @@
 # oS: ohne Steckbrett
 # oR: ohne Reflektor
 
+
 def walzeDrehen(walze):
+	'''Die Funktion walzeDrehen verschiebt jedes Element eines Arrays um eine Stelle nach rechts'''
 	tmp = walze[1:len(walze)]
 	tmp.append(walze[0])
 	walze = tmp
 	return walze
 
-# Die Funktion walzeDrehen verschiebt jedes Element eines Arrays um eine Stelle nach rechts
 
 
-w0 = [3,8,5,1,7,2,6,4] # Die Verdrahtung der Walzen kann durch simple Arrays dargestellt werden.
+# Die Verdrahtung der Walzen kann durch simple Arrays dargestellt werden.
+
+# Benutzerdefinierte Verdrahtung der Walzen [DEBUG]
+# walzen = []
+# frage = 'Bitte eine beliebige Reihenfolge der Zahlen 1-8 eingeben, in der jede Zahl ein einziges Mal vorkommt: \n'
+# for i in range(3):
+# 	walzen.append(list(raw_input(frage)))
+# w0 = walzen[0]
+# w1 = walzen[1]
+# w2 = walzen[2]
+
+# Im Quellcode festgelegte Walzenverdrahtung [HISTORY]
+w0 = [3,8,5,1,7,2,6,4] 
 w1 = [8,1,7,3,6,5,2,4]
 w2 = [4,2,6,7,3,8,5,1]
 
 
-klartext = '163826572354263245232547467567583562462523451'
+# Benutzerdefinierter Klartext [HISTORY]
+klartext = raw_input('Bitte eine Kombination von Zahlen von 1 bis 8 eingeben: \n')
+
+# Im Quellcode festgelegter Klartext [DEBUG][NoHistory]
+# klartext = '163826572354263245232547467567583562462523451'
+
+
 klartextListe = list(klartext)
 geheimtext = ''
 
@@ -27,7 +46,7 @@ counterW0 = 0 # Diese Counter zählen die Durchläufe der Walzen
 counterW1 = 0
 counterW2 = 0
 
-for i in xrange(len(klartext)):
+for counter in xrange(len(klartext)):
 	if counterW0 > 8:
 		w1 = walzeDrehen(w1)
 		counterW1 += 1
@@ -45,8 +64,6 @@ for i in xrange(len(klartext)):
 	
 	w0 = walzeDrehen(w0)
 	counterW0 += 1
+	
 print geheimtext
-	
-	
-	
 		 
--- README.md
+++ README.md
@@ -14,6 +14,9 @@ Enigma and other rotor cipher maschines
    (Auch in anderen Projekten zu finden)
 7. Wird eine for-Schleife zum zählen verwendet, so heißt die Zählvariable "counter".
 8. Kommentare werden mit "#" erstellt (Ausnahme: Docstrings!)
+###### Dateinamenkonvention:
+Die komischen Dateinamen kommen so zusammen:
+<Kürzel des historischen Vorbilds>.W<Walzenzahl>.<Steckerbrett?>.<Reflektor>.py 
 
 #### Anhänge:
 In manchen Codedateien wird auf Anhänge verwiesen, welche im Ordner "Anhang" zu finden sind.
@@ -21,10 +24,13 @@ In manchen Codedateien wird auf Anhänge verwiesen, welche im Ordner "Anhang" zu
 #### TODO:
 * [HISTORY]   Veränderungsmöglichkeit der Walzenlage durch den Benutzer 
 * [HISTORY]   Eingabe von Buchstaben
-*             Möglichkeit zufallsgenerierter Walzenanordnungen (siehe Anhang 1)
+*             Möglichkeit zufallsgenerierter Walzenanordnungen (siehe Anhang A)
 * [HISTORY]   Möglichkeit die Startposition der Walzen festzulegen
 * [NoHistory] Variable Walzenzahl
 * [DEBUG]     Häufigkeitsanalyse des Geheimtextes
 * [DEBUG]     Speichermöglichkeit der Walzen
+
+#### /tmp/
+* Die Dateien ENI.W5.oS.R.py und ENI.W8.oS.oR.py müssen noch an die Konventionen angepasst werden.
 
 
